5c582ddff7a01d408c96369dfd29a16101afccaf
Ryan P <ryanp424@gmail.com>
2017-12-10 13:48:17 -0800

    trying to get gobot to work (code needs piglow driver), and also updatedTaskfile

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+
 )
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Hello world!!!")
+
+	r := raspi.NewAdaptor()
+	led := gpio.NewLedDriver(r, "4")
+
+	work := func() {
+			gobot.Every(1*time.Second, func() {
+					led.Toggle()
+			})
+	}
+
+	robot := gobot.NewRobot("blinkBot",
+			[]gobot.Connection{r},
+			[]gobot.Device{led},
+			work,
+	)
+
+	robot.Start()
 }
